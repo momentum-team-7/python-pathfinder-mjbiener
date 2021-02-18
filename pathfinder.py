14da347a0b45d315b6f97167a74fcd8488226785
mjbiener <michaelbiener@yahoo.com>
2021-02-18 14:29:48 -0500

    grayscale picture renders

--- pathfinder.py
+++ pathfinder.py
@@ -1,3 +1,7 @@
+from PIL import Image
+small = 'elevation_small.txt'
+large = 'elevation_large.txt'
+
 coordinates = []
 
 with open('elevation_small.txt', 'r') as file:
@@ -7,10 +11,12 @@ with open('elevation_small.txt', 'r') as file:
 
 # need to find the min value in the list (will be set to black 0, 0, 0)
 # need to find the max value in the list (will be set to white 255, 255, 255)
-# difference between max and min will be scaled to difference between 255-0
 
-max_el = int(max(map(max, coordinates)))
-print(max_el)
+# def get_max_el ():
+    max_el = int(max(map(max, coordinates)))
+    print(max_el)
+    # return max_el
+    
 
 min_el = int(min(map(min, coordinates)))
 print(min_el)
@@ -18,9 +24,28 @@ print(min_el)
 el_delta = int((max_el) - (min_el))
 print (el_delta)
 
+# difference between max and min needs to be scaled to difference between 255-0
 
 
-from PIL import Image
-img = Image.new('RGB', (600, 600), color = (200, 200, 200))
-img.save ('solid_color_image.png')
+def calc_color_value (elevation, min, max) :
+    color_value = (((int(elevation) - min_el)/(el_delta)) * 255)
+    print(int(color_value), int(color_value), int(color_value))
+    return (int(color_value), int(color_value), int(color_value))
 
+# determines the pixel dimensions of the required picture 
+dimensions = len(coordinates), len(coordinates[0])
+print (dimensions)
+
+
+
+# img = Image.new('RGB', dimensions, color = (200, 200, 200))
+# img.save ('solid_color_image.png')
+
+
+img = Image.new('RGB', dimensions,)
+for x in range(dimensions[0]):
+        for y in range(dimensions[1]):
+            img.putpixel((x, y), calc_color_value(
+                coordinates[x][y], min_el, max_el))
+# img.save('mountain_image.png')
+img.save(small[0:len(small)-3] + 'png')
